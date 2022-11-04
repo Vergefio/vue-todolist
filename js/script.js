8b246adb3f5dbf10671eda3a846f64cf2d363b68
Vergefio <riccardods@live.it>
2022-11-04 17:25:04 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            tasks: [
+                {
+                text: 'Prendere la mappa di Artù',
+                do: 'false'      
+            },
+                {
+                text: 'Studiare Vue',
+                do: 'true'       
+            },
+                {
+                text: 'Fare il bucato',
+                do: 'false'     
+            },
+        ]
+        }
+    },
+
+    methods:{
+        // creo (nome) funzione e mi riprendo l'indice per andarlo a controllare
+        toogleDone (index) {
+            if (this.tasks[index].do){
+                // specificare da dove partire tipo
+                this.tasks.splice(index, 1);
+            }
+        }
+    }
+
+}).mount('#app');
